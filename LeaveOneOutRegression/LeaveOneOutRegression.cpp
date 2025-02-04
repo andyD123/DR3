7738e5ea4a6956da94a61c084fa5976041b993eb
andyD123 <andreedrakeford@hotmail.com>
2025-02-04 00:48:41 +0000

    initial commit  loo regression

--- LeaveOneOutRegression/LeaveOneOutRegression.cpp
+++ LeaveOneOutRegression/LeaveOneOutRegression.cpp
@@ -12,15 +12,16 @@
 #include <vector>
 #include <iomanip>
 #include <functional>
+#include <chrono>
 
 
 
 //using namespace DRC::VecD2D; 
 //using namespace DRC::VecF4F;
 //using namespace DRC::VecD4D;
-using namespace DRC::VecD8D;
+//using namespace DRC::VecD8D;
 //using namespace DRC::VecF16F;
-//using namespace DRC::VecF8F;
+using namespace DRC::VecF8F;
 
 
 
@@ -29,9 +30,9 @@ int main()
 {
 
 
-    std::vector<double> vc(300, 1.1);
+   // std::vector<double> vc(300, 1.1);
 
-    VecXX myvec = vc;
+  //  VecXX myvec = vc;
 
     int N = 1000000;
 
@@ -41,10 +42,12 @@ int main()
     for (int i = 0; i < N; i++)
     {
         data_X[i] = i;
-        data_Y[i] = 0.5 * i + 0.25;// +rand() / double(RAND_MAX);
+        data_Y[i] = 0.5 * i + 0.25 +rand() / double(RAND_MAX);
     }
 
+  //  std::vector<double>  debg;
 
+  const auto  startTme = std::chrono::high_resolution_clock::now();
 
     auto MULT = [](auto x, auto y) { return x * y; };
     auto SUM = [](auto x, auto y) { return x + y; };
@@ -58,49 +61,59 @@ int main()
     auto S_xy = transformReduce(data_X, data_Y, MULT, SUM);
 
     auto SX_loo = S_x - data_X;
+
+ //   debg = SX_loo;
+
     auto SY_loo = S_y - data_Y;
+
+//    debg = SY_loo;
 
     auto data_X_squared  = data_X * data_X;
     auto SXxx_loo = S_xx - data_X_squared;
 
+ //   debg = SXxx_loo;
+
     auto data_X_Y = data_X * data_Y;
     auto SXY_loo = S_xy - data_X_Y;
+
+  //  debg = SXY_loo;
 
 
     ///
     double lambda = 0.0;// 0.1;
-    double Sz = data_X.size();
+    double Sz = data_X.size()-1.0;
 
-    auto denominator = Sz * (SXxx_loo + lambda) + SX_loo * SX_loo;
+    auto denominator = (Sz * (SXxx_loo + lambda)) - (SX_loo * SX_loo);
 
     auto Beta_0_numerator = (SXxx_loo + lambda) * SY_loo - SX_loo * SXY_loo;
 
     auto Beta_0 = Beta_0_numerator / denominator;
 
-    auto Beta_1_numerator = Sz * SXY_loo - (SXxx_loo * SY_loo);
+    auto Beta_1_numerator = Sz * SXY_loo - (SX_loo * SY_loo);
 
     auto Beta_1 = Beta_1_numerator / denominator;
 
+ //   debg = denominator;
+ //   debg = Beta_0_numerator;
+ //  debg = Beta_1_numerator;
 
-    std::vector<double> offset = Beta_0;
-    std::vector<double> slope = Beta_1;
-    std::vector<double> x = data_X;
-    std::vector<double> y = data_Y;
+ //   std::vector<double> offset = Beta_0;
+ //   std::vector<double> slope = Beta_1;
+ //   std::vector<double> x = data_X;
+ //   std::vector<double> y = data_Y;
 
 
+   const auto endTme = std::chrono::high_resolution_clock::now();
+   
+  //  const std::chrono::duration<double, std::milli> runtime = endTme - startTme;
+  
+   // auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(runtime);
 
+    const std::chrono::duration<double, std::milli> fp_ms = endTme - startTme;
 
+    // integral duration: requires duration_cast
+   // const auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
 
-    std::cout << "Hello World!\n";
+    std::cout << fp_ms.count() <<  " milli seconds";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
